5c1f91b0e11737331d83ecd668fcd5d53a3d3088
Chris Arter <chrisarter@Chriss-MBP.home>
2020-12-01 10:29:56 -0500

    day uno

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace advent_of_code_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Day1()).solve();
+        }
+    }
+}
